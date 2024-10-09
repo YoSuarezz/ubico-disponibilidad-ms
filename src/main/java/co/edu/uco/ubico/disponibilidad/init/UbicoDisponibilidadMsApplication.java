1c1f480d89204f873f61a08dff3d759543d09fb5
Sebastian <sebassuarez103@gmail.com>
2024-10-09 18:58:05 -0500

    Inicializacion del proyecto, conexion con la base de datos, implementacion de mapper

---
+++ src/main/java/co/edu/uco/ubico/disponibilidad/init/UbicoDisponibilidadMsApplication.java
@@ -0,0 +1,13 @@
+package co.edu.uco.ubico.disponibilidad.init;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UbicoDisponibilidadMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UbicoDisponibilidadMsApplication.class, args);
+	}
+
+}
